fbac411a54e59aa4a22af4ca462b11312901c8bc
Pedro3179 <leonevescer@gmail.com>
2026-07-10 15:26:50 -0300

    Refine comments and var names

--- hanoi_tower.py
+++ hanoi_tower.py
@@ -1,16 +1,12 @@
-# Remember to consider edge cases and check if the input is an integer
-# Remove var step
-
-
-# Calculate max number of moves to solve the challenge
+# Calculate the maximum number of moves
 def calculate_steps(number):
     if not isinstance(number, int):
-        raise TypeError('Input must be an integer.')
+        raise TypeError("Input must be an integer.")
 
     return 2**number - 1
 
 
-# Check whether an array is reverse sorted:
+# Check whether an array is reverse-sorted:
 def is_reverse_sorted(arr):
     if not arr:
         return True
@@ -23,12 +19,13 @@ def is_reverse_sorted(arr):
 
 ### MAIN ###
 
+
 def hanoi_solver(total_disks):
     # Check if the input is an integer
     if not isinstance(total_disks, int):
-        raise TypeError('Input must be an integer.')
+        raise TypeError("Input must be an integer.")
 
-    # Find where the target disk should move
+    # Find where the selected disk should move
     def find_target_rod_pos(current_rod_pos, previous_target):
         positions = (0, 1, 2)
 
@@ -37,25 +34,25 @@ def hanoi_solver(total_disks):
                 continue
 
             return pos
-        
-    # Generate the list of numbers in the first rod
-    first_rod = list(
-        range(total_disks, 0, -1)
-    ) 
 
-    position_map = {}  # Records the initial index of each number in the tower
+    # Generate the list of numbers in the first rod
+    first_rod = list(range(total_disks, 0, -1))
+
+    position_map = {}  # Stores the rod position of each disk
 
     for n in first_rod:
         position_map[n] = 0
 
-    hanoi_tower = [list(first_rod), [], []]
+    hanoi_tower = [
+        list(first_rod),
+        [],
+        [],
+    ]  # Tower state that will be modified while solving the puzzle
 
-    output_string=f'{hanoi_tower[0]} {hanoi_tower[1]} {hanoi_tower[2]}'
+    output_string = f"{hanoi_tower[0]} {hanoi_tower[1]} {hanoi_tower[2]}"  # Output produced by hanoi_solver()
 
-    # Divide and revert sort the tower of hanoi
-    def reverse_sort(
-            array, tower, position_map, output_string, previous_target=1
-            ):
+    # Recursively reverse-sort the tower of hanoi
+    def reverse_sort(array, tower, position_map, output_string, previous_target=1):
         target_number_rod_pos = position_map.get(array[0])
         target_rod = find_target_rod_pos(target_number_rod_pos, previous_target)
         previous_target = target_rod
@@ -72,41 +69,35 @@ def hanoi_solver(total_disks):
 
             tower[disk_pos].remove(disk)
 
-            output_string+=f'\n{tower[0]} {tower[1]} {tower[2]}'
+            output_string += f"\n{tower[0]} {tower[1]} {tower[2]}"
 
             position_map[disk] = target_rod  # Records the change in the position map
             # print(f"Position map after change: {position_map}")
 
             return output_string
 
-        target_number = array[0]  # Take the disk to be moved
-        rest = array[1:]  # take the rest to be sorted
+        target_number = array[0]  # Select the disk to move
+        rest = array[1:]  # Remaining disks to process
 
-        # Divide the the array until it is one number and sort
-        output_string=reverse_sort(rest, tower, position_map, output_string, previous_target)
+        # Recursively split the array until it is only one disk and sort it
+        output_string = reverse_sort(
+            rest, tower, position_map, output_string, previous_target
+        )
 
-
-        # Loop through the array
+        # Move each disk to the target rod
         for disk in array:
             disk_pos = position_map.get(disk)
 
             current_rod = tower[disk_pos]
             current_last_disk = min(current_rod)
 
-            # If the disk is not the last item, subdivide again
+            # If the disk is not the last item, recursively split again
             if current_last_disk != disk:
                 disk_pos_current_rod = current_rod.index(disk)
                 rest = current_rod[disk_pos_current_rod + 1 :]
-                # print(f'Rest: {rest}')
 
-                # print(f'Previous target in the loop: {previous_target}')
-
-                output_string=reverse_sort(
-                    rest,
-                    tower, 
-                    position_map,
-                    output_string,
-                    previous_target
+                output_string = reverse_sort(
+                    rest, tower, position_map, output_string, previous_target
                 )
 
             if disk == target_number:
@@ -117,7 +108,7 @@ def hanoi_solver(total_disks):
 
                 tower[disk_pos].remove(disk)
 
-                output_string+=f'\n{tower[0]} {tower[1]} {tower[2]}'
+                output_string += f"\n{tower[0]} {tower[1]} {tower[2]}"
 
                 position_map[disk] = (
                     target_rod  # Records the change in the position map
@@ -133,8 +124,8 @@ def hanoi_solver(total_disks):
                 disk_pos = position_map.get(disk)
 
                 tower[disk_pos].remove(disk)
-                
-                output_string+=f'\n{tower[0]} {tower[1]} {tower[2]}'
+
+                output_string += f"\n{tower[0]} {tower[1]} {tower[2]}"
 
                 position_map[disk] = (
                     target_rod  # Records the change in the position map
@@ -142,16 +133,16 @@ def hanoi_solver(total_disks):
 
         return output_string
 
-    #print(f"Last Array is reverse sorted: {is_reverse_sorted(hanoi_tower[2])}")
+    # print(f"Last rod is reverse-sorted: {is_reverse_sorted(hanoi_tower[2])}")
 
     return reverse_sort(first_rod, hanoi_tower, position_map, output_string)
 
+
 n = 5
 
-ht=hanoi_solver(n)
+ht = hanoi_solver(n)
 
 print(ht)
 
 max_steps = calculate_steps(n)
-#print(f"Max number of steps: {max_steps}")
-
+# print(f"Max number of steps: {max_steps}")
